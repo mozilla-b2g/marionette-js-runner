c1284cd1dde5d885553ca2f37742c7e86c087044
James Lal <james@lightsofapollo.com>
2013-07-25 15:04:39 -0700

    Bug 892260 - new test filtering api: marionette('name', { filter: xxx });

--- test/runtime/filterdata.js
+++ test/runtime/filterdata.js
@@ -1,37 +1,65 @@
 suite('runtime/filterdata', function() {
-  var FilterData = require('../../lib/runtime/filterdata').FilterData;
-  var filter, potential_metadata1, potential_metadata2, potential_metadata3,
-    potential_metadata4, potential_metadata5, potential_metadata6;
+  var subject = require('../../lib/runtime/filterdata').FilterData;
 
-  setup(function() {
-    filter = {foo: 1, bar: "2", host: ["firefox", "b2g-desktop", "device"]};
-    potential_metadata1 = {host: "firefox", foo: 1, bar: "2"};
-    potential_metadata2 = {host: "firefox", foo: 1, bar: 2};
-    potential_metadata3 = {host: 1, bar: "2", arr: ["firefox"]};
-    potential_metadata4 = {host: "firefox", foo: "2", bar: "2"};
-    potential_metadata5 = {host: "firefox", foo: 1, bar: "2", hello: {}};
-    potential_metadata6 = {host: "firefox", foo: 1};
+  suite('#validateArray', function() {
+    test('is in array', function() {
+      assert.ok(subject.validateArray([1, 2], 2));
+    });
+
+    test('not in array', function() {
+      assert.ok(!subject.validateArray([1, 2], 4));
+    });
   });
 
-  test('validate', function() {
-    assert.equal(FilterData.validate(filter, filter), false);
-    assert.equal(FilterData.arrayCheck(filter.host, filter.host), false);
-    assert.equal(FilterData.validate(filter, potential_metadata1), true);
-    assert.equal(FilterData.arrayCheck(filter.host, potential_metadata1.host), true);
-    assert.equal(FilterData.any(filter.host, potential_metadata1.host), false);
-    assert.equal(FilterData.validate(filter, potential_metadata2), false);
-    assert.equal(FilterData.arrayCheck(filter.host, potential_metadata2.host), true);
-    assert.equal(FilterData.any(filter.host, potential_metadata2.host), false);
-    assert.equal(FilterData.validate(filter, potential_metadata3), false);
-    assert.equal(FilterData.arrayCheck(filter.host, potential_metadata3.host), false);
-    assert.equal(FilterData.validate(filter, potential_metadata4), false);
-    assert.equal(FilterData.arrayCheck(filter.host, potential_metadata4.host), true);
-    assert.equal(FilterData.any(filter.host, potential_metadata4.host), false);
-    assert.equal(FilterData.validate(filter, potential_metadata5), true);
-    assert.equal(FilterData.arrayCheck(filter.host, potential_metadata5.host), true);
-    assert.equal(FilterData.any(filter.host, potential_metadata5.host), false);
-    assert.equal(FilterData.validate(filter, potential_metadata6), false);
-    assert.equal(FilterData.arrayCheck(filter.host, potential_metadata6.host), true);
-    assert.equal(FilterData.any(filter.host, potential_metadata6.host), false);
+  suite('#validate', function() {
+    function verify(name, filter, metadata, result) {
+      test(name, function() {
+        assert.equal(subject.validate(filter, metadata), result);
+      });
+    }
+
+    function valid(name, filter, metadata) {
+      verify(name, filter, metadata, true);
+    }
+
+    function invalid(name, filter, metadata) {
+      verify(name, filter, metadata, false);
+    }
+
+    valid('empty filter and metadata', {}, {}, true);
+    valid('empty fitler', {}, { host: 'firefox' }, true);
+
+    valid(
+      'single valid filter option',
+      { host: 'firefox' },
+      { host: 'firefox' }
+    );
+
+    invalid('missing property in metadata', { host: 'firefox' }, {});
+    invalid('mismatched properties', { host: 'a' }, { host: 'b' });
+
+    valid(
+      'array filter values (metadata property must match one value in array)',
+      { host: ['a', 'b', 'c'] },
+      { host: 'c' }
+    );
+
+    invalid(
+      'metadata property exists but not in array',
+      { host: ['a', 'b'] },
+      { host: 'c' }
+    )
+
+    valid(
+      'filter requires multiple properties',
+      { host: ['a', 'b'], wifi: true },
+      { host: 'a', wifi: true }
+    );
+
+    invalid(
+      'partial matching of metadata',
+      { host: 'a', wifi: true },
+      { host: 'a' }
+    );
   });
 });
